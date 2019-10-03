1e0e6970d646f6922f7aba84d772cc4ed599a712
Marco Antonio Gonçalves Pegoraro <magpegoraro@ibm.com>
2019-10-03 11:57:12 -0300

    Finished frontend

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import api from '../../services/api'
+import {Link} from 'react-router-dom'
 
 import './styles.css'
 
@@ -26,10 +27,15 @@ export default function Dashboard() {
                 <li key={spot._id}>
                     <header style={{backgroundImage: `url(${spot.thumbnail_url})`}}/>
                     <strong>{spot.company}</strong>
-                    <span>{spot.price}</span>
+                    <span>{spot.price ? `R$${spot.price}/dia`: 'Gratuito'}</span>
                 </li>
             ))}
         </ul>
+        <Link to="/new">
+            <button className="btn">
+            Cadastrar novo spot
+            </button>
+        </Link>
         </>
     )
 }
